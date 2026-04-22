397eea8c9ceb05dde0b658e49eabb295d808d5a4
ED <edgartozlian@gmail.com>
2026-04-22 11:54:53 +0300

    обновлены структуры

--- Структуры/Структуры.cpp
+++ Структуры/Структуры.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-const int SIZE = 100;
+/*const int SIZE = 100;
 struct inv_type 
 {
     char item[40];
@@ -119,7 +119,256 @@ int main()
         }
 
     }
+}*/
+
+
+const int SIZE = 7;
+struct routes
+{
+    char name[80];
+    char dest[80];
+    int route_n;
+}aeroflot[SIZE];
+void aeroflot_init()
+{
+    int t;
+    for (t = 0; t < SIZE; t++)
+        *aeroflot[t].name = '\0';
 }
+char aeroflot_menu()
+{
+    char ch;
+    cout << '\n';
+    do
+    {
+        system("cls");
+        cout << "(E)nter\n";
+        cout << "(D)isplay\n";
+        cout << "(S)elected display";
+        cout << "(U)pdate\n";
+        cout << "(Q)uit\n\n";
+        cout << "Выберите команду: "; cin >> ch;
+    } while (!strchr("eduq", tolower(ch)));
+    return tolower(ch);
+}
+
+void aeroflot_input(int i)
+{
+    cout << "Название самолета: "; cin >> aeroflot[i].name;
+    cout << "Номер рейса: "; cin >> aeroflot[i].route_n;
+    cout << "Пункт назначения: "; cin >> aeroflot[i].dest;
+    
+}
+void aeroflot_enter()
+{
+    int i;
+    cout << '\n';
+    for (i = 0; i < SIZE; i++)
+        if (!*aeroflot[i].name) break;
+    if (i == SIZE)
+    {
+        cout << "Список полон.\n"; return;
+    }
+    aeroflot_input(i);
+}
+void aeroflot_update()
+{
+    int i;
+    char name[80];
+    cout << "Введите наименование товара: ";
+    cin >> name;
+    for (i = 0; i < SIZE; i++)
+        if (!strcmp(name, aeroflot[i].name))
+            break;
+    if (i == SIZE)
+    {
+        cout << "Товар не найден.\n"; return;
+    }
+    cout << "Введите новую информацию.\n";
+    aeroflot_input(i);
+}
+
+void aeroflot_display()
+{
+    int t;
+
+    for (t = 0; t < SIZE; t++)
+    {
+        if (*aeroflot[t].name)
+        {
+            cout << "\n\nНазвание самолета: " << aeroflot[t].name << '\n';
+            cout << "Номер рейса: " << aeroflot[t].route_n << '\n';
+            cout << "Пункт назначения: " << aeroflot[t].dest << '\n';
+        }
+    }
+    system("pause");
+
+}
+
+void aeroflot_selected_display()
+{
+    int t;
+    char name[80];
+    cout << "Введите тип самолета: ";
+    cin.ignore();
+   
+    for (t = 0; t < SIZE; t++)
+    {
+        if (*aeroflot[t].name)
+        {
+            cout << "\n\nНазвание самолета: " << aeroflot[t].name << '\n';
+            cout << "Номер рейса: " << aeroflot[t].route_n << '\n';
+            cout << "Пункт назначения: " << aeroflot[t].dest << '\n';
+        }
+    }
+    system("pause");
+}
+
+void AEROFLOT()
+{
+    system("chcp 1251");
+    bool quit = false;
+    char choice;
+    aeroflot_init();
+    while(!quit)
+    {
+        choice = aeroflot_menu();
+
+        switch (choice)
+        {
+        case 'e':
+            aeroflot_enter();
+            break;
+        case 'd':
+            aeroflot_display();
+            break;
+        case 's':
+            aeroflot_selected_display();
+            break;
+        case 'u':
+            aeroflot_update();
+            break;
+        case 'q':
+            quit = true;
+            break;
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void task_2()
+{
+
+}
+
+void task_3()
+{
+
+}
+
+void task_4()
+{
+
+}
+
+void task_5()
+{
+
+}
+
+void task_6()
+{
+
+}
+
+
+
+int menu()
+{
+    int choice;
+    cout << "** Меню **\n";
+    cout << "1. AEROFLOT\n";
+    cout << "2. e\n";
+    cout << "3. Функция для квадратного уравнения\n";
+    cout << "4. Функция для выставления нижнего регистра строки\n";
+    cout << "5. Функции для работы с дробями\n";
+    cout << "6. Функция для возврата простых делителей\n";
+    cout << "0. Выход\n";
+    cout << "Выберите действие: ";
+    cin >> choice;
+    return choice;
+}
+
+int main()
+{
+    system("chcp 1251");
+    int choice;
+    do
+    {
+        system("cls");
+        choice = menu();
+        switch (choice)
+        {
+        case 1:
+            system("cls");
+            AEROFLOT();
+            system("pause");
+            break;
+        case 2:
+            system("cls");
+            task_2();
+            system("pause");
+            break;
+        case 3:
+            system("cls");
+            task_3();
+            system("pause");
+            break;
+        case 4:
+            system("cls");
+            task_4();
+            system("pause");
+            break;
+        case 5:
+            system("cls");
+            task_5();
+            system("pause");
+            break;
+        case 6:
+            system("cls");
+            task_6();
+            system("pause");
+            break;
+        case 0:
+            system("cls");
+            break;
+        default:
+            cout << "\n ОШИБКА";
+            break;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+
+
+
+
 
     
 
