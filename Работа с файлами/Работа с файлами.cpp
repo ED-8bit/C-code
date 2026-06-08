bad5f50c273a7f9c6c956e2cee1377ae963a8a79
ED <edgartozlian@gmail.com>
2026-06-08 23:33:29 +0300

    files

--- Работа с файлами/Работа с файлами.cpp
+++ Работа с файлами/Работа с файлами.cpp
@@ -1,11 +1,40 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <filesystem>
 using namespace std;
 
 int main()
 {
-	ofstream out;
-	out.open("file.txt");
-	out.close();
+	int ch;
+	system("chcp 1251");
+	system("cls");
+	cout << "Path: " << filesystem::current_path() << '\n';
+	cout << "1. write\n";
+	cout << "2. erase\n";
+	cout << "choice: ";
+	cin >> ch;
+	ofstream fout;
+	switch(ch)
+	{
+	case 1:
+		fout.open("file.txt", ios::app);
+		if (fout.is_open())
+		{
+			fout << "Hello world!" << '\n';
+			fout.close();
+			cout << "\nWrited" << '\n';
+		}
+		else
+			cout << "\nError" << '\n';
+		break;
+	case 2:
+		fout.open("file.txt");
+		if (fout.is_open())
+		{
+			fout << " ";
+		}
+		break;
+	}
+	
 	return 0;
 }
